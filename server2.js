2b5d38ca61da839884fe6b248e18c780df076d8d
leonardodadenchik <63752204+leonardodadenchik@users.noreply.github.com>
2022-02-17 19:37:47 +0200

    cool animation

--- server2.js
+++ server2.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + "/page"));
 app.post("/user",jsonParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    response.json(request.body);
+    response.json(12);
 });
 
 app.listen(3000, () => console.log("Сервер работает"));

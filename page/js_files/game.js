fc39a41d38488fa118f13838ab8dffc23a2725d2
leonardodadenchik <persidzekson228507@gmail.com>
2022-03-25 15:58:04 +0200

    finaly

--- page/js_files/game.js
+++ page/js_files/game.js
@@ -1,32 +1,56 @@
-async function req_func(data_to_send) {
-    let response = await fetch("/get_values", {
+let HOST = location.origin.replace(/^http/, 'ws')
+let myWs = new WebSocket(HOST);
+
+
+let game_data = {
+    cards: "",
+    room_code: "",
+    nicknames: "",
+}
+
+async function req_func(data_to_send, link) {
+    let response = await fetch(link, {
         method: 'POST',
         headers: {
-            'Content-Type':'application/json;charset=utf-8'
+            'Content-Type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify(data_to_send)
     });
     let result = await response.json();
-
+    app.room_code = `your room code is: ${result.code}`;
     console.log(result);
-    /*
-    let innering = '';
-    for(elem of result){
-
-        innering = innering + elem.situation + '\n'
-    }
-    document.getElementById('wait').innerText = innering
-    */
 }
 
-
-function send_room_data(){
-     // document.getElementById("sit_id_s").value;
+function send_room_data() {
     let room_settings = {
         player_count: document.getElementById("player_count").value,
         card_count: document.getElementById("card_count").value,
         sit_count: document.getElementById("sit_count").value,
-        nicknames: document.getElementById("nicknames").value,
     };
-    req_func(room_settings);
+    req_func(room_settings, "/get_values");
+    app.go_to_connection();
+}
+window.onbeforeunload = wsClose;
+function wsClose() {
+    myWs.send(JSON.stringify({ action: 'disconnect', room_name: game_data.room_code }));
+}
+function connect_to_room() {
+    let room_name = document.getElementById("room_name").value;
+    game_data.room_code = room_name;
+    let player_name = document.getElementById("player_name").value;
+
+    myWs.send(JSON.stringify({ action: 'connect_to_room', code: room_name.toString(), name: player_name.toString() }));
+
+
+    myWs.onmessage = function (message) {
+        message = JSON.parse(message.data)
+        switch (message.action) {
+            case "message":
+                break;
+            default:
+                console.log(message.message);
+                break;
+
+        }
+    };
 }
--- server2.js
+++ server2.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-
-const PORT = process.env.PORT || 3000
-const jsonParser = express.json()
-const fs = require('fs');
+const WebSocket = require('ws');
+const wsServer = new WebSocket.Server({ port: 9000 });
 
 const { MongoClient } = require('mongodb')
 const client = new MongoClient('mongodb+srv://kaelovek:letmekeepitsecret@cluster0.y4hpw.mongodb.net/memesituations?retryWrites=true&w=majority')
+
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3000
+const jsonParser = express.json()
 
 const { generatorCode } = require('./module_func')
 const { different_nums } = require('./module_func')
@@ -18,13 +21,11 @@ const { card_gen } = require('./module_func')
 const photo_dir = './page/images_library'
 const collection_situations = 'situations'
 const collection_rooms = 'rooms'
-const code_length = 4;
 const allPictureFiles = getAllDirPhotoFiles(photo_dir, fs)
 
-
+client.connect()
 
 app.use(express.static(__dirname + "/page"));
-
 app.post("/get_values", jsonParser, async function (request, response) {
     if (!request.body) return response.sendStatus(400);
 
@@ -66,3 +67,63 @@ app.post("/get_values", jsonParser, async function (request, response) {
 })
 
 app.listen(PORT, () => console.log("Сервер работает"));
+
+
+wsServer.on('connection', onConnect);
+
+function onConnect(wsClient) {
+
+    console.log('Новое подключение');
+
+    wsClient.on('close', function () {
+        console.log('Пользователь отключился');
+    });
+
+
+    wsClient.on('message', function (message) {
+        try {
+            const jsonMessage = JSON.parse(message);
+            switch (jsonMessage.action) {
+                case 'connect_to_room':
+                    let roomVariants = async () => {
+                        let room = await client.db().collection(collection_rooms).findOne({ code: jsonMessage.code })
+                        if (!!room) {
+                            let players = room.players
+                            if (players.length === room.playersCount) {
+                                wsClient.send(JSON.stringify({ action: message, message: 'room already  fool lel -- =_=' }));
+                            } else {
+                                wsClient.send(JSON.stringify({ action: message, message: `cards: ${room.cards[players.length]}` }))
+                                players.push(
+                                    {
+                                        name: jsonMessage.name,
+                                        wsClient: wsClient
+                                    })
+                                await client.db().collection(collection_rooms).updateOne(
+                                    { code: room.code },
+                                    {
+                                        $set: {
+                                            players: players
+                                        }
+                                    })
+                            }
+                        } else {
+                            wsClient.send(JSON.stringify({ action: message, message: 'room not  exists 404 -- 0_0' }))
+                        }
+                    }
+                    roomVariants()
+                    // jsonMessage.code --> room id
+                    // jsonMessage.name --> p name add
+                    //wsClient --> add
+                    break;
+                case 'disconnect':
+                    console.log(jsonMessage.room_name);
+                    break;
+                default:
+                    console.log('Неизвестная команда');
+                    break;
+            }
+        } catch (error) {
+            console.log('Ошибка', error);
+        }
+    });
+}

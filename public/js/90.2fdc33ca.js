aa4235b0ed2c9016fbd52fbf3745b59734c840d6
denvery31 <denvery31@gmail.com>
2022-05-19 15:47:03 +0300

    1st Start

---
+++ public/js/90.2fdc33ca.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[90],{2090:function(e,r,n){n.r(r),n.d(r,{default:function(){return s}});var t=n(3396);function u(e,r,n,u,a,o){return(0,t.wg)(),(0,t.iD)("div")}var a={created(){this.$router.push({name:"join",params:{propCode:`${this.$route.params.roomCode}`}})}},o=n(89);const i=(0,o.Z)(a,[["render",u]]);var s=i}}]);
+//# sourceMappingURL=90.2fdc33ca.js.map

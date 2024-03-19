a6d5acca179ab9dcd44b884190ac0af03c9002c1
leonardodadenchik <persidzekson228507@gmail.com>
2024-03-19 16:23:29 -0400

    http and websocket run on the same port now!

--- client/src/assets/myWs/myWs.js
+++ client/src/assets/myWs/myWs.js
@@ -1,6 +1,8 @@
-const host = (location.origin.replace(/^http/, 'ws') + ':80/')
+const host = location.origin.replace(/^http/, 'ws')
+
+/*const host = (location.origin.replace(/^http/, 'ws') + ':80/')
     .replace(':3000', '')
-    .replace(':8080', '')
+    .replace(':8080', '')*/
 const myWs = new WebSocket(host)
 
 export default myWs
